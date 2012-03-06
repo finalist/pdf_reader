c3c5cb1254795f8aaf9d0b8ee2680349bd7b32c1
iain <iain@iain.nl>
2012-03-06 22:27:39 +0100

    Add pending spec for #2

--- spec/pdf_reader_spec.rb
+++ spec/pdf_reader_spec.rb
@@ -1,8 +1,16 @@
 Dir.glob(File.join(File.dirname(__FILE__), '..', 'lib', '*.rb')).each { |f| require f }
 
 describe PDFReader do
+
   it "should extract raw text from pdf" do
     p = PDFReader.new('spec/test.pdf')
     p.raw_text.should eql('Test PDF, try and read me!')
   end
+
+  it "handles header checks" do
+    pending
+    p = PDFReader.new('spec/troublesome.pdf')
+    p.raw_text.should include "Release Note"
+  end
+
 end
